100296b49f15a2300301b4436089c4aac9ffce46
taonick102-maker <taonick102@gmail.com>
2025-10-17 14:25:49 +0700

    Rename TrangChu.html to index.html

--- JavaCript (TT)/FromSv.js
+++
@@ -1,8 +0,0 @@
-function Ktrhote(HOTEN){
-    let tu = HOTEN.trim().spilit(/\s+/);
-    return tu.length >= 2; 
-}
-function ktrEmail(Mail){
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
